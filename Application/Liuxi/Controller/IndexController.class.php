e8a10738c727d659d1967ac162f68f4df5c1d6dd
myskies <3792535@qq.com>
2015-11-11 21:16:52 +0800

    排序作业Ｃ层

---
+++ Application/Liuxi/Controller/IndexController.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace Liuxi\Controller;
+
+use Think\Controller;
+use Liuxi\Logic\LiuxiLogic;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        $lists = array();
+        $i = 0;
+
+        do {
+            $lists[] = rand(1, 1000);
+        } while ( $i++ <= 20);
+
+        echo "Source data is " . implode(",", $lists);
+        echo "<br />";
+
+        $QuickSortL = new LiuxiLogic();
+        $lists = $QuickSortL->sort($lists);
+        
+        echo "Sorted data is " . implode(",", $lists);
+        echo "<br />";
+
+    }
+}
