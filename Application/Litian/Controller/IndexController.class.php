0222b05fa40b55359b208939e3b4fa208cb0e685
litian1218 <1181551049@qq.com>
2015-11-14 16:46:47 +0800

    补齐之前的代码

--- Application/Litian/Controller/IndexController.class.php
+++ Application/Litian/Controller/IndexController.class.php
@@ -1,27 +1,128 @@
 <?php
 namespace Litian\Controller;
+
 use Think\Controller;
 use Litian\Logic\LitianLogic;
 
 class IndexController extends Controller
 {
-    public function indexAction()
-    {
-        $lists = array();
-        $i = 0;
+    public function indexAction(){
+        //取值
+        $UserL = new LitianLogic();
+        $Users =$UserL->getAllLists();
 
-        do {
-            $lists[] = rand(1, 1000);
-        } while ( $i++ <= 20);
+        //传值
+        $this ->assign('users',$Users);
 
-        echo "Source data is " . implode(",", $lists);
-        echo "<br />";
+        //前台显示
+        $this->display();
+    }
+    public function detailAction(){
+        //取用户ID
+        $userId = I('get.id');
 
-        $QuickSortL = new LitianLogic();
-        $lists = $QuickSortL->QuickSort($lists);
+        //抓取用户信息
+        $UserL = new LitianLogic();
+        $user = $UserL->getListById($userId);
 
-        echo "Sorted data is " . implode(",", $lists);
-        echo "<br />";
+        //传值
+        $this->assign('user',$user);
+
+        $this->display();
+    }
+    public function addAction(){
+
+        $this->display();
+    }
+    public function saveAction(){
+        //取用户信息
+        $userId=I('post.');
+
+        //添加add
+        $UserL= new LitianLogic();
+        $UserL=addList($user);
+
+        //判断异常
+        if(count($errors=$UserL->getErrors())!==0)
+        {
+            //将数组的值转化为字符串
+            $error=implode('<br/>',"$errors");
+
+            //显示错误
+            $this->error("添加失败，原因：".$error,U('Litian/Index/index'));
+        }
+        else
+        {
+            $this->success("添加成功",U('Litian/Index/index'));
+        }
+    }
+    public function editAction(){
+        //取用户ID
+        $UserId=I('get.id');
+        //取用户信息
+        $UserL = new LitianLogic();
+        $user = $UserL->getListById($UserId);
+        //dump($user)
+        //传值
+        $this->assign('user',$user);
+
+        //显示页面
+        $this->display('edit');
+    }
+    public function updateAction(){
+        //取用户信息
+        $data = I('post.id');
+
+        //传给M层
+        $UserL = new LitianLogic();
+        $UserL->save($data);//调用PHP的save方法
+
+        //判断异常
+        if(count($errors=$UserL->getErrors())!== 0){
+            //数组变字符串
+            $error = implode('<br/>',$errors);
+            //显示错误
+            $this->error("添加失败，原因：".$error,U('Litian/Index/index'));
+            return false;
+        }
+        else{
+            $this->success("操作成功",U('Litian/Index/index'));
+        }
 
     }
+    public function deleteAction(){
+        //取ID信息
+        $userId = I('get.id');
+
+        //调用M层方法
+        $UserL = new LitianLogic();
+        $status = $UserL->deleteInfo($userId);
+
+        //判断状态
+        if($status !==false){
+           $this->success("删除成功", U('Litian/Index/index'));
+        }
+        else{
+            $this->error("删除失败" , U('Litian/Index/index'));
+        }
+    }
+    // public function indexAction()
+    // {
+    //     $lists = array();
+    //     $i = 0;
+
+    //     do {
+    //         $lists[] = rand(1, 1000);
+    //     } while ( $i++ <= 20);
+
+    //     echo "Source data is " . implode(",", $lists);
+    //     echo "<br />";
+
+    //     $QuickSortL = new LitianLogic();
+    //     $lists = $QuickSortL->QuickSort($lists);
+
+    //     echo "Sorted data is " . implode(",", $lists);
+    //     echo "<br />";
+
+    // }
 }
--- Application/Litian/Logic/LitianLogic.class.php
+++ Application/Litian/Logic/LitianLogic.class.php
@@ -1,10 +1,84 @@
 <?php
-namespace QuickSort\Logic;
+namespace Litian\Logic;
 
-use Think\Model\LitianModel;
+use Litian\Model\LitianModel;
 
 class LitianLogic extends LitianModel
 {
+    //保护函数
+    protected $errors = array();
+
+    //错误信息
+    public function getErrors(){
+        return $this ->errors;
+    }
+
+
+    public function getListByName($name){
+        //去空格
+        $named = trim($name," ");
+
+        //判断是否为空
+        if ($named == ""){
+            $this ->errors[]="用户名不能为空";
+        }
+
+        //判断是否是字符串
+        else if(is_string($named)!==true){
+            $this ->errors[]="请输入正确的用户名";
+        }
+
+        //判断是否为关键字
+        else if($named=="yunzhi"){
+            $this->errors[]="不能查找yunzhi关键字";
+        }
+
+        //函数
+        $map['name'] = $named;
+        $data = $this->where($map)->find();
+        return $data;
+    }
+
+    //取编辑页
+    public function getListById($id){
+        $map['id']=$id;
+        $data = $this->where($map)->find();
+        return $data;
+    }
+
+    //取页面
+    public function getAllLists(){
+        $datas = $this->select();
+        return $datas;
+    }
+
+    //删除
+    public function deleteInfo($id){
+        $map['id']=$id;
+        $data= $this->where($map)->find();
+        return $data;
+    }
+    //增加数据时验证
+    public function addList($list){
+        try{
+            if($this->create($list))
+            {
+                $id=$this->add();
+                return $id;
+            }
+            else{
+                $this->errors[]=$this->getError();
+                return false;
+            }
+        }
+        catch(Think\Exception $e)
+        {
+            $this->errors[]=$e->getMessage();
+            //getMessage()是PHP的一个捕获异常信息的方法
+            return false;
+        }
+    }
+    //快速排序函数
     public function QuickSort()
     {
         //判断数组长度 0或1直接输出
