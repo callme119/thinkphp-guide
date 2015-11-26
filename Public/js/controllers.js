381b59f7c29344c8d18a64cee8fedd4e3d3e7078
mskies <3792535@qq.com>
2015-11-26 16:21:31 +0800

    checkbox

--- Public/js/controllers.js
+++ Public/js/controllers.js
@@ -1,18 +1,16 @@
-app.controller('refreshCtrl', 
-    ['$scope', '$timeout', function($scope, $timeout){
-        //为前面绑定数据
-        $scope.items = [
-        {name:"shijiangcheng"},
-        {name:"xingyannian"},
-        {name:"panjie"},
+app.controller('checkboxCtrl', 
+    ['$scope',
+    function($scope){
+        $scope.devList = [
+        {text:"HTML5",checked:true},
+        {text:"CSS3",checked:false},
+        {text:"JavaScript",checked:false}
         ];
 
-        //定义doRefresh函数
-        $scope.doRefresh = function(){
-            //未使用HTTP，在这，延迟2S，模拟GET操作。
-            $timeout(function(){}, 2000).then(function(){
-                $scope.items.push({name:"xulinjie"});
-                $scope.$broadcast('scroll.refreshComplete');
-            });
-        }
-}])
+        $scope.pushNotificationChange = function() {
+            console.log("changed", $scope.pushNotification.checked);
+        };
+
+        $scope.pushNotificaton = { checked: true};
+        $scope.emailNotification = "Subscribed";
+    }]);
