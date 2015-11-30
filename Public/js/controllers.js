a0607f2d733d5ae0d98083933eb245dedfe84221
mskies <3792535@qq.com>
2015-11-30 10:24:43 +0800

    ionic-router

--- Public/js/controllers.js
+++ Public/js/controllers.js
@@ -1,42 +1,66 @@
-app.controller('checkboxCtrl', 
-    ['$scope',
-    function($scope){
-        $scope.devList = [
-        {text:"HTML5",checked:true},
-        {text:"CSS3",checked:false},
-        {text:"JavaScript",checked:false}
-        ];
 
-        $scope.pushNotificationChange = function() {
-            console.log("changed", $scope.pushNotification.checked);
-        };
 
-        $scope.pushNotificaton = { checked: true};
-        $scope.emailNotification = "Subscribed";
-    }]);
+app.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+    .state('tabs',
+    {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+    })
+    .state("tabs.about",{
+        url: "/about",
+        views:{
+            "about-tab":{
+                templateUrl: "templates/about.html"
+            }
+        }
+    })
+    .state('tabs.home',{
+        url: "/home",
+        views:{
+            'home-tab':{
+                templateUrl: "templates/home.html",
+                controller: "HomeTabCtrl"
+            }
+        }
+    })
+    .state("tabs.facts",{
+        url: "/facts",
+        views: {
+            "home-tab":{
+                templateUrl: "templates/facts.html"
+            }
+        }
+    })
+    .state("tabs.facts2",{
+        url: "/fact2",
+        views:{
+            'home-tab':{
+                templateUrl: "templates/facts2.html"
+            }
+        }
+    })
 
-app.controller('MyCtrl', 
-    ['$scope', 
-    '$ionicGesture', 
-    function($scope, $ionicGesture){
-    $scope.lastEventCall = "请尝试在主体内容上进行拖拽";
-    var events = [
-        {event: "dragup",text:"触发了向上拖拽"},
-        {event: "dragdown",text:"触发了向下拖拽"},
-        {event: "dragleft",text:"触发了向左拖拽"},
-        {event: "dragright",text:"触发了向右拖拽"},
-        {event: "swipeup",text:"触发了向上滑动"},
-        {event: "swipedown",text:"触发了向下滑动"},
-        {event: "swipeleft",text:"触发了向左滑动"},
-        {event: "swiperight",text:"触发了向右滑动"}
-        ];
+    .state('tabs.navstack', {
+          url: "/navstack",
+          views: {
+            'about-tab': {
+              templateUrl: "templates/nav-stack.html"
+            }
+          }
+        })
+        .state('tabs.contact', {
+          url: "/contact",
+          views: {
+            'contact-tab': {
+              templateUrl: "templates/contact.html"
+            }
+          }
+        });
+       $urlRouterProvider.otherwise("/tab/about");
+});
 
-    var element = angular.element(document.querySelector("#eventPlaceholder"));
-    angular.forEach(events, function(value, key){
-            $ionicGesture.on(value.event, function(event){
-                $scope.$apply(function(){
-                    $scope.lastEventCall = value.text;
-                });
-            },element);
-    });
-}])
+app.controller("HomeTabCtrl", ['$scope', function(){
+    console.log('HomeTabCtrl');
+}]);
