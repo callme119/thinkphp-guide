f9887aadaae716748dd76bee63c324f29c1e48a7
githubXLJ <164408119@qq.com>
2015-11-30 20:28:51 +0800

    所有界面的Interface模块儿，有的界面还没有写进去

---
+++ Application/Interface/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Ionic\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        $this->display();
+    }
+}
