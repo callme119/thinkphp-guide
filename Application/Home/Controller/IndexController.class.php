f056df831ad7372ccc7c3071bc2ed039a8ae0c96
githubXLJ <164408119@qq.com>
2015-12-02 11:03:02 +0800

    改HomePage为Home

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,27 +1,12 @@
 <?php
 namespace Home\Controller;
+
 use Think\Controller;
-use User\Logic\UserLogic;		//
+
 class IndexController extends Controller
 {
-	public function indexAction()
-	{
-		//判断用户是否登陆
-		$userId = I('session.userId');
-		if($userId == '')
-		{
-			$this->success("请登录",U('Login/Index/index'));
-			exit();
-		}
-		
-		//取用户信息
-		$UserL = new UserLogic();
-		$user = $UserL->getListById($userId);
-		
-		//传值 
-		$this->assign("user",$user);
-
-
-		$this->display();
-	}
+    public function indexAction()
+    {
+        $this->display();
+    }
 }
