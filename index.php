8104601a8a6b7c84d4b20cbb3a7ff4e6243fff4c
githubXLJ <164408119@qq.com>
2015-12-06 16:40:19 +0800

    测试

--- index.php
+++ index.php
@@ -9,8 +9,46 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-// 应用入口文件
+// weixin应用入口文件
+//初始化消息回复
+$wechatObj = new wechatCallbackapiLogic();
 
+if (isset($_GET['echostr'])) {
+	$wechatObj->valid();
+}else{
+	echo "hello";
+}
+
+class wechatCallbackapiLogic {
+	//初始化
+	public function valid()
+	{
+		$echoStr = $_GET['echostr'];
+		if($this->checkSignature()){
+			echo $echoStr;
+			exit();
+		}
+	}
+
+    //教研签名
+	private function checkSignature()
+	{
+		$signature = $_GET['signature'];
+		$timestamp = $_GET['timestamp'];
+		$nonce = $_GET['nonce'];
+
+		$token = "yunzhi";
+		$tmpArr =  array($token, $timestamp,$nonce);
+		sort($tmpArr);
+		$tmpStr = implode($tmpArr);
+		$tmpStr = sha1($tmpStr);
+
+		if( $tmpStr == $signature){
+			return true;
+		}else{
+			return false;
+		}
+	}
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
