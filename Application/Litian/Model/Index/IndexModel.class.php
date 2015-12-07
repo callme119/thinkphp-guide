a8521d9a4a454076c3e7b24980850eabda2b6b46
litian1218 <1181551049@qq.com>
2015-12-07 10:44:23 +0800

    修改get

---
+++ Application/Litian/Model/Index/IndexModel.class.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Litian\Model\Index;
+
+use User\Model\UserModel;
+
+/**
+*
+*/
+class IndexModel extends UserModel
+{
+
+    public function getLists()
+    {
+        # code...
+    }
+}
