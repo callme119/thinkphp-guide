fe194f8512325a3cc412fcb6f4e527e72ea9bce7
weijingyun <1028193951@qq.com>
2015-12-08 16:01:17 +0800

    page标签

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'YUNZHI_PAGE_SIZE'      => 2,          //默认分页大小
+    'YUNZHI_PAGE_SIZE'      => 3,          //默认分页大小
     
 	'DEFAULT_MODULE'        => 'Login',  // 默认模块
     'ACTION_SUFFIX'         => 'Action',//设置ACTION前缀为action
@@ -9,7 +9,7 @@ return array(
    	'DB_PREFIX' => 'yunzhi_', // 数据库表前缀
    	'DB_TYPE'   => 'mysqli', // 数据库类型
     'DB_HOST'   => '127.0.0.1',
-    'DB_NAME'   => 'wjytest', // 数据库名performancems
+    'DB_NAME'   => 'test', // 数据库名performancems
     'DB_USER'   => 'root', // 用户名performancems
     'DB_PWD'    => '',  // 密码b2408cac49ed15d67c390dd08a8b0158
     'DB_PORT'   => '3306', // 端口3306);
--- Application/User/Controller/SampleController.class.php
+++ Application/User/Controller/SampleController.class.php
@@ -3,14 +3,24 @@ namespace User\Controller;
 
 use Think\Controller;
 use Wjy\Model\UserModel;
+use User\Logic\UserLogic;		//用户表
+use User\Model\Index\indexModel;  
 
 Class SampleController extends Controller
 {
 	public function indexAction()
 	{
-		$UserM = new UserModel();
+		//获取列表
+        $UserM = new UserModel();
 		$users = $UserM->getLists();
-		dump($users);
+
+        $IndexModel = new indexModel();
+        $IndexModel->setUsers($users);
+
+        //传入列表
+    	$this->assign('M',$IndexModel);
+        
+    	
 		$this->display();
 	}
 
