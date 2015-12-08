26c2274471c8f553454508c46105b3ba3770123b
litian1218 <1181551049@qq.com>
2015-12-08 21:11:10 +0800

    修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,8 +1,10 @@
 <?php
 return array(
+    'YUNZHI_PAGE_SIZE'      => 2,          //默认分页大小
+
 	'DEFAULT_MODULE'        => 'Login',  // 默认模块
     'ACTION_SUFFIX'         => 'Action',//设置ACTION前缀为action
-    'TAGLIB_PRE_LOAD'       =>  'Yunzhi', //引用YUNZHI标签库
+    'TAGLIB_PRE_LOAD'       => 'Yunzhi', //引用YUNZHI标签库
 
    	'DB_PREFIX' => 'yunzhi_', // 数据库表前缀
    	'DB_TYPE'   => 'mysqli', // 数据库类型
@@ -11,7 +13,8 @@ return array(
     'DB_USER'   => 'root', // 用户名performancems
     'DB_PWD'    => '',  // 密码b2408cac49ed15d67c390dd08a8b0158
     'DB_PORT'   => '3306', // 端口3306);
-    'YUNZHI_PAGE_SIZE' => '4',//分页中，每页显示的条数使用C(PAGE_SIZE)读取;
+    'PAGE_SIZE' => '4',//分页中，每页显示的条数使用C(PAGE_SIZE)读取;
+
     /* 模板相关配置 */
     'TMPL_PARSE_STRING' => array(
         '__IMG__'       => __ROOT__ . '/Public/img',
